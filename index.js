ba37ed0bd3b10c263913ce45632c6b61e24f0a3b
Victor Virgolino <victorvirgolino@gmail.com>
2021-07-23 10:46:18 -0300

    dois endpoints /mensagens e /mensagens:id

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const porta = 3000;
 
+const mensagens = [
+  "mensagem 1",
+  "mensagem 2"  
+];
+//[GET] / hello - manda um hello world
 app.get('/hello', (req, res) =>{
     res.send("Hello World");
+})
+//[GET] /mensagens - envia as mensagens
+app.get('/mensagens', (req,res) =>{
+    res.send(mensagens);
+})
+//[GET] /mensagens:id - retorna uma mensagem
+app.get('/mensagens/:id', (req,res) =>{
+    const id = req.params.id;
+    const mensagem = mensagens[id];
+    res.send(mensagem)
 })
 
 
